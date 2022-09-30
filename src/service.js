79fbe2de75ecbf4f1a79cfb269ea656e4ab8f9f7
SalJelena <salopekjelena24@gmail.com>
2022-09-30 13:47:43 +0200

    Fixed Sass styles and BEM naming of the elements

--- src/service.js
+++ src/service.js
@@ -23,40 +23,13 @@ export const data = [
     },
 ]
 
-
-//ubacivanje itema u niz
-
-export const add = (item)=> {   //napomena: objekat item sadrzi samo desc i done
-    item.id = count++
- 
+export const add = (item)=> {
+    item.id = count++ 
     data.push(item)
-    //data.push({id:count++,...item})
-   // return count - 1
-
    return item.id
 }
-
-
-
-//brisanje iz niza
 
 export const deleteById =  id => {
     let index = data.findIndex(item=>item.id == id)
     data.splice(index, 1)
 }
-
-
-
-
-
-// //zamena u nizu  -- nije bilo navedeno u domacem zadatku da treba da se uradi
-
-// export const changeById = (id, noviItem)=>{
-//     let index = data.findIndex(item=>item.id == id)
-//     data.splice(index,1,noviItem)
-// }
-
-
-
-
-
